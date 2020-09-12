221b9e1b1360b41ffce1f7f8f0dcb4c9038b6b05
heavensward.argentlux <heavensward.argentlux@gmail.com>
2020-09-12 17:00:16 +0800

    finish initial google review scrape funciton

---
+++ scraper/google-review-scraper.js
@@ -0,0 +1,32 @@
+const puppeteer = require('puppeteer');
+
+let scrape = async(url) => {
+	const browser = await puppeteer.launch({args: ['--no-sandbox', '--disabled-setuid-sandbox']});
+	const page = await browser.newPage();
+
+	await page.goto(url);
+	await page.waitForSelector('.widget-pane-visible')
+
+	const result = await page.evaluate(() => {
+		let fullName = document.querySelector('.section-review-title').innerText;
+		let postDate = document.querySelector('.section-review-publish-date').innerText;
+		let starRating = document.querySelector('.section-review-stars').getAttribute('aria-label');
+		let postReview = document.querySelector('.section-review-text').innerText;
+
+		return {
+			fullName,
+			postDate,
+			starRating,
+			postReview
+		}
+	});
+
+	browser.close();
+	return result;
+};
+
+let url = 'https://www.google.com/maps/place/Nexus+Clinic+Kuala+Lumpur+(Skin+%26+Hair+Aesthetic+Clinic)/@3.1524901,101.7099621,17z/data=!3m1!4b1!4m7!3m6!1s0x0:0x24ca69d549f44339!8m2!3d3.1524901!4d101.7121508!9m1!1b1'
+
+scrape(url).then((result) => {
+	console.log(result);
+})
