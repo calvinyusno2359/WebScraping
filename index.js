48e5af8aa01a503609e66f6eaa5acae758897679
aaronreulkhoo <aaronkhoo@live.com>
2020-09-13 19:20:22 +0800

    Scraping function finished

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ let greviewscraper = require('./scraper/google-review-scraper');
 
 let urls = [
 	'https://www.google.com/maps/place/Nexus+Clinic+Kuala+Lumpur+(Skin+%26+Hair+Aesthetic+Clinic)/@3.1524901,101.7099621,17z/data=!3m1!4b1!4m7!3m6!1s0x0:0x24ca69d549f44339!8m2!3d3.1524901!4d101.7121508!9m1!1b1',
-	'https://www.google.com/maps/place/Medi+Aesthetic+-+Aesthetic+Clinic+Johor+Bahru/@1.5318751,103.7900154,17z/data=!3m1!4b1!4m7!3m6!1s0x0:0x7dee5772424cfede!8m2!3d1.5318751!4d103.7922041!9m1!1b1'
-	]
+	// 'https://www.google.com/maps/place/Medi+Aesthetic+-+Aesthetic+Clinic+Johor+Bahru/@1.5318751,103.7900154,17z/data=!3m1!4b1!4m7!3m6!1s0x0:0x7dee5772424cfede!8m2!3d1.5318751!4d103.7922041!9m1!1b1'
+	];
 
 for (url of urls) {
 	greviewscraper.scrape(url).then(result => {
--- scraper/google-review-scraper.js
+++ scraper/google-review-scraper.js
@@ -8,20 +8,20 @@ let scrape = async(url) => {
 	const reviewPage = await browser.newPage();
 
 	await reviewPage.goto(url, {waitUntil: 'networkidle2'});
-	await reviewPage.waitForSelector('.widget-pane-visible')
+	await reviewPage.waitForSelector('.widget-pane-visible');
+
 
 	const metadata = await reviewPage.evaluate(() => {
-		let aggregateRating = document.querySelector('.gm2-display-2').innerText;
-		let totalReviews = document.querySelector('.gm2-caption').innerText;
-
+		let aggregateRating = document.querySelector('div.gm2-display-2').innerText;
+		let totalReviews = Number(document.querySelector('div.gm2-caption').innerText.split(" ")[0]);
 		return {
 			aggregateRating,
 			totalReviews,
 		}
-	})
+	});
 
-	// NOTE: nned to wrap this in a loop, to get all reviews
-	const reviews = await reviewPage.evaluate(() => {
+	// NOTE: need to wrap this in a loop, to get all reviews
+	/*const reviews = await reviewPage.evaluate(() => {
 		let fullName = document.querySelector('.section-review-title').innerText;
 		let postDate = document.querySelector('.section-review-publish-date').innerText;
 		let starRating = document.querySelector('.section-review-stars').getAttribute('aria-label');
@@ -33,7 +33,67 @@ let scrape = async(url) => {
 			starRating,
 			postReview
 		}
+	});*/
+
+
+	// Scroll till all reviews loaded
+	let countReviews = await reviewPage.evaluate(() => {
+		return Number(document.querySelectorAll(".section-review-content").length);
 	});
+
+	while (countReviews < metadata.totalReviews) {
+
+		let prevHeight = await reviewPage.evaluate(() => {
+			return document.querySelector(".section-scrollbox.scrollable-y.scrollable-show").scrollHeight;
+		});
+		await reviewPage.evaluate(() => {
+			let scrollArea = document.querySelector(".section-scrollbox.scrollable-y.scrollable-show");
+			scrollArea.scrollTo(0, scrollArea.scrollHeight);
+		});
+		await reviewPage.waitForFunction(`document.querySelector('.section-scrollbox.scrollable-y.scrollable-show').scrollHeight>${prevHeight}`);
+		await reviewPage.waitForTimeout(100);
+		countReviews = await reviewPage.evaluate(() => {
+			return document.querySelectorAll(".section-review-content").length;
+		});
+	}
+
+	// Scrape
+	const reviews = await reviewPage.evaluate(() => {
+		let countReviews = Number(document.querySelectorAll(".section-review-content").length);
+		// Fetch all reviews
+		let fullNameElements = document.getElementsByClassName('section-review-title');
+		let fullNames = [];
+		for (let elements of fullNameElements) {
+			fullNames.push(elements.innerText);
+		}
+		let postDateElements = document.getElementsByClassName('section-review-publish-date');
+		let postDates = [];
+		for(let elements of postDateElements) {
+			postDates.push(elements.innerText);
+		}
+
+		let starRatingElements = document.getElementsByClassName('section-review-stars');
+		let starRatings = [];
+		for (let elements of starRatingElements) {
+			starRatings.push(elements.getAttribute('aria-label').charAt(1));
+		}
+
+		let postReviewElements = document.getElementsByClassName('section-review-text');
+		let postReviews = [];
+		for(let elements of postReviewElements) {
+			postReviews.push(elements.innerText);
+		}
+
+		return {
+			countReviews,
+			fullNames,
+			postDates,
+			starRatings,
+			postReviews
+		}
+	});
+
+	// TODO: Export to csv for analysis
 
 	browser.close();
 
